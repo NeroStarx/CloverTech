94fddf2bf6920dbc49baa0e72f92e11fecee0192
Aaron NeroStarx <35573892+NeroStarx@users.noreply.github.com>
2020-06-23 18:21:52 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('wilaya')->nullable();
-            $table->string('region')->nullable();
+            $table->string('grade')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
